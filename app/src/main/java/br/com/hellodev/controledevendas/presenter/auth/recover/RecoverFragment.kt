70eb2202245de3570f9de71685f439e8b31f6ca4
Arley Santana <arley1995@ucl.br>
2022-06-06 21:34:33 -0300

    Adicionado opção para resetar a senha.

--- app/src/main/java/br/com/hellodev/controledevendas/presenter/auth/recover/RecoverFragment.kt
+++ app/src/main/java/br/com/hellodev/controledevendas/presenter/auth/recover/RecoverFragment.kt
@@ -1,14 +1,18 @@
 package br.com.hellodev.controledevendas.presenter.auth.recover
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import br.com.hellodev.controledevendas.R
 import br.com.hellodev.controledevendas.databinding.FragmentRecoverBinding
+import br.com.hellodev.controledevendas.util.BaseFragment
+import br.com.hellodev.controledevendas.util.FirebaseHelper
 import br.com.hellodev.controledevendas.util.initToolbar
+import br.com.hellodev.controledevendas.util.showBottomSheet
 
-class RecoverFragment : Fragment() {
+class RecoverFragment : BaseFragment() {
 
     private var _binding: FragmentRecoverBinding? = null
     private val binding get() = _binding!!
@@ -25,6 +29,39 @@ class RecoverFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar(binding.toolbar)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnRecover.setOnClickListener {
+            val email = binding.edtEmail.text.trim().toString()
+
+            if (email.isNotEmpty()) {
+
+                hideKeyboard()
+
+                binding.progressBar.isVisible = true
+
+                recoverAccount(email)
+
+            } else {
+                showBottomSheet(message = getString(R.string.email_empty_recover_fragment))
+            }
+        }
+    }
+
+    private fun recoverAccount(email: String) {
+        FirebaseHelper
+            .getAuth()
+            .sendPasswordResetEmail(email)
+            .addOnSuccessListener {
+                binding.progressBar.isVisible = false
+                showBottomSheet(message = getString(R.string.text_send_sucess_recover_fragment))
+            }.addOnFailureListener {
+                binding.progressBar.isVisible = false
+                showBottomSheet(message = getString(FirebaseHelper.validateError(it.message.toString())))
+            }
     }
 
     override fun onDestroyView() {
